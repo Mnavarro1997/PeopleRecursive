020268b7a9a63d178aa58dcc6da6419b8ec39ca1
MNavarro1997 <mnavarro@gteam.es>
2021-09-09 14:00:50 +0200

    Agregar archivos de proyecto.

---
+++ PeopleRecursive/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PeopleRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] People = new string[5] { "Juan", "Pedro", "Maria", "Clara", "Fernando" };
+
+            for (int i = 0; i < People.Length; i++)
+            {
+                Console.WriteLine(People[i]);
+
+            }
+    
+        }
+        public void Saludar()
+        {
+
+        }
+    }
+}
